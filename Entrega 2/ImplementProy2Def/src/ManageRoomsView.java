22668425ff2277028bdf0239f2ca9b999437bfdd
juanrs10 <jm.ramirezs1@uniandes.edu.co>
2023-05-09 21:56:36 -0500

    ManageRoom and SetPriceView windows created

--- Entrega 2/ImplementProy2Def/src/ManageRoomsView.java
+++ Entrega 2/ImplementProy2Def/src/ManageRoomsView.java
@@ -1,18 +1,70 @@
-import java.io.*;
-import java.util.*;
-import java.io.FileReader;
-import java.io.IOException;
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
+import java.awt.*;
 import javax.swing.*;
-
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
 
 class ManageRoomsView {
+
     public void showWindow() {
         JFrame frame = new JFrame("Manage Rooms");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(500, 500);
+        frame.setSize(500, 600);
+
+        JPanel panel = new JPanel(new GridLayout(8,2));
+
+        JLabel roomNumberLabel = new JLabel("Room Number:");
+        JTextField roomNumberField = new JTextField();
+
+        JLabel locationLabel = new JLabel("Location:");
+        JTextField locationField = new JTextField();
+
+        JLabel capacityLabel = new JLabel("Capacity:");
+        JTextField capacityField = new JTextField();
+
+        JLabel typeLabel = new JLabel("Type:");
+        String[] types = {"Standard", "Suite", "Double Suite"};
+        JComboBox<String> typeField = new JComboBox<>(types);
+
+        JLabel featuresLabel = new JLabel("Additional Features:");
+        JCheckBox balconyCheck = new JCheckBox("Balcony");
+        JCheckBox viewCheck = new JCheckBox("View");
+        JCheckBox kitchenCheck = new JCheckBox("Integrated Kitchen");
+
+        JButton createRoomButton = new JButton("Create Room");
+        createRoomButton.addActionListener(e -> {
+            // Handle room creation
+        });
+
+        JButton loadRoomsButton = new JButton("Load Rooms From File");
+        loadRoomsButton.addActionListener(e -> {
+            JFileChooser chooser = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("CSV Files", "csv");
+            chooser.setFileFilter(filter);
+            int returnVal = chooser.showOpenDialog(frame);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = chooser.getSelectedFile();
+                // Handle file loading
+            }
+        });
+
+        panel.add(roomNumberLabel);
+        panel.add(roomNumberField);
+        panel.add(locationLabel);
+        panel.add(locationField);
+        panel.add(capacityLabel);
+        panel.add(capacityField);
+        panel.add(typeLabel);
+        panel.add(typeField);
+        panel.add(featuresLabel);
+        panel.add(balconyCheck);
+        panel.add(new JLabel(""));
+        panel.add(viewCheck);
+        panel.add(new JLabel(""));
+        panel.add(kitchenCheck);
+        panel.add(createRoomButton);
+        panel.add(loadRoomsButton);
+
+        frame.add(panel);
         frame.setVisible(true);
     }
 }
-
--- Entrega 2/ImplementProy2Def/src/SetPriceView.java
+++ Entrega 2/ImplementProy2Def/src/SetPriceView.java
@@ -1,17 +1,54 @@
-import java.io.*;
-import java.util.*;
-import java.io.FileReader;
-import java.io.IOException;
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
+import java.awt.*;
 import javax.swing.*;
 
-
 class SetPricesView {
+
     public void showWindow() {
         JFrame frame = new JFrame("Set Prices");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(500, 500);
+        frame.setSize(500, 600);
+
+        JPanel panel = new JPanel(new GridLayout(7, 2));
+
+        JLabel roomTypeLabel = new JLabel("Room Type:");
+        String[] types = {"Standard", "Suite", "Double Suite"};
+        JComboBox<String> roomTypeField = new JComboBox<>(types);
+
+        JLabel dateRangeLabel = new JLabel("Date Range:");
+        JPanel dateRangePanel = new JPanel(new GridLayout(1,2));
+        JSpinner startDateField = new JSpinner(new SpinnerDateModel());
+        JSpinner endDateField = new JSpinner(new SpinnerDateModel());
+        dateRangePanel.add(startDateField);
+        dateRangePanel.add(endDateField);
+
+        JLabel daysOfWeekLabel = new JLabel("Days of Week:");
+        JPanel daysOfWeekPanel = new JPanel(new GridLayout(1, 7));
+        JCheckBox[] dayChecks = new JCheckBox[7];
+        String[] daysOfWeek = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        for (int i = 0; i < 7; i++) {
+            dayChecks[i] = new JCheckBox(daysOfWeek[i]);
+            daysOfWeekPanel.add(dayChecks[i]);
+        }
+
+        JLabel priceLabel = new JLabel("Price:");
+        JTextField priceField = new JTextField();
+
+        JButton setPriceButton = new JButton("Set Price");
+        setPriceButton.addActionListener(e -> {
+            // Handle price setting
+        });
+
+        panel.add(roomTypeLabel);
+        panel.add(roomTypeField);
+        panel.add(dateRangeLabel);
+        panel.add(dateRangePanel);
+        panel.add(daysOfWeekLabel);
+        panel.add(daysOfWeekPanel);
+        panel.add(priceLabel);
+        panel.add(priceField);
+        panel.add(setPriceButton);
+
+        frame.add(panel);
         frame.setVisible(true);
     }
 }
